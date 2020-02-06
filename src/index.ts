d092b4937ec4c77560b2c3db9782aba321f82a6a
K2France <60738267+K2France@users.noreply.github.com>
2020-02-06 13:49:54 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,45 @@
 import '@k2oss/k2-broker-core';
 
 metadata = {
-    systemName: "com.k2.example",
-    displayName: "Example Broker",
-    description: "An example broker that accesses JSONPlaceholder."
+    systemName: "com.k2.countries",
+    displayName: "Countries Broker",
+    description: "An example broker that accesses Countries."
 };
 
-ondescribe = async function(): Promise<void> {
+ondescribe = async function (): Promise<void> {
     postSchema({
         objects: {
-            "com.k2.todo": {
-                displayName: "TODO",
-                description: "Manages a TODO list",
+            "com.k2.country": {
+                displayName: "Get List of countries",
+                description: "Get List of countries from rest api",
                 properties: {
-                    "com.k2.todo.id": {
-                        displayName: "ID",
-                        type: "number"
-                    },
-                    "com.k2.todo.userId": {
-                        displayName: "User ID",
-                        type: "number"
-                    },
-                    "com.k2.todo.title": {
-                        displayName: "Title",
+                    "com.k2.country.name": {
+                        displayName: "Name",
                         type: "string"
                     },
-                    "com.k2.todo.completed": {
-                        displayName: "Completed",
-                        type: "boolean"
+                    "com.k2.country.alpha2Code": {
+                        displayName: "Alpha2 Code",
+                        type: "string"
+                    },
+                    "com.k2.country.capital": {
+                        displayName: "Capital",
+                        type: "string"
+                    },
+                    "com.k2.country.nativeName": {
+                        displayName: "Native Name",
+                        type: "string"
+                    },
+                    "com.k2.country.flag": {
+                        displayName: "Flag",
+                        type: "string"
                     }
                 },
                 methods: {
-                    "com.k2.todo.get": {
-                        displayName: "Get TODO",
+                    "com.k2.country.list": {
+                        displayName: "Get Countries",
                         type: "read",
-                        inputs: [ "com.k2.todo.id" ],
-                        outputs: [ "com.k2.todo.id", "com.k2.todo.userId", "com.k2.todo.title", "com.k2.todo.completed" ]
+                        inputs: ["com.k2.country.id"],
+                        outputs: ["com.k2.country.id", "com.k2.country.userId", "com.k2.country.title", "com.k2.country.completed"]
                     }
                 }
             }
@@ -43,37 +47,34 @@ ondescribe = async function(): Promise<void> {
     });
 }
 
-onexecute = async function(objectName, methodName, _parameters, properties): Promise<void> {
-    switch (objectName)
-    {
-        case "com.k2.todo": await onexecuteTodo(methodName, properties); break;
+onexecute = async function (objectName, methodName, _parameters, properties): Promise<void> {
+    switch (objectName) {
+        case "com.k2.country": await onexecuteTodo(methodName, properties); break;
         default: throw new Error("The object " + objectName + " is not supported.");
     }
 }
 
 async function onexecuteTodo(methodName: string, properties: SingleRecord): Promise<void> {
-    switch (methodName)
-    {
-        case "com.k2.todo.get": await onexecuteTodoGet(properties); break;
+    switch (methodName) {
+        case "com.k2.country.list": await onexecutecountrieslist(properties); break;
         default: throw new Error("The method " + methodName + " is not supported.");
     }
 }
 
-function onexecuteTodoGet(properties: SingleRecord): Promise<void> {
-    return new Promise<void>((resolve, reject) =>
-    {
+function onexecutecountrieslist(properties: SingleRecord): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
         var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
+        xhr.onreadystatechange = function () {
             try {
                 if (xhr.readyState !== 4) return;
                 if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
 
                 var obj = JSON.parse(xhr.responseText);
                 postResult({
-                    "com.k2.todo.id": obj.id,
-                    "com.k2.todo.userId": obj.userId,
-                    "com.k2.todo.title": obj.title,
-                    "com.k2.todo.completed": obj.completed
+                    "com.k2.country.name": obj.name,
+                    "com.k2.country.alpha2Code": obj.alpha2Code,
+                    "com.k2.country.capital": obj.capital,
+                    "com.k2.country.flag": obj.flag
                 });
                 resolve();
             } catch (e) {
@@ -81,8 +82,8 @@ function onexecuteTodoGet(properties: SingleRecord): Promise<void> {
             }
         };
 
-        xhr.open("GET", 'https://jsonplaceholder.typicode.com/todos/' + properties["com.k2.todo.id"]);
-        xhr.setRequestHeader('test', 'test value');
+        xhr.open("GET", 'https://restcountries.eu/rest/v2/all');
+        //xhr.setRequestHeader('test', 'test value');
         xhr.send();
     });
 }
